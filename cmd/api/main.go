2806a7b419e1defe050ad876883eda67a783c43c
nicholaswijaya004 <danielwijaya2004@gmail.com>
2026-09-01 02:50:09 +0700

    feat: graceful shutdown on SIGINT and SIGTERM

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
@@ -27,9 +34,34 @@ func main() {
 		IdleTimeout:  60 * time.Second,
 	}
 
-	logger.Info("starting server", "addr", srv.Addr)
-	if err := srv.ListenAndServe(); err != nil {
+	serverErr := make(chan error, 1)
+	go func() {
+		logger.Info("starting server", "addr", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			serverErr <- err
+		}
+	}()
+
+	select {
+	case err := <-serverErr:
 		logger.Error("server failed", "error", err)
 		os.Exit(1)
+	case <-ctx.Done():
+		logger.Info("shutdown signal received")
 	}
+
+	stop()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		logger.Error("graceful shutdown failed, forcing close", "error", err)
+		if err := srv.Close(); err != nil {
+			logger.Error("force close failed", "error", err)
+		}
+		os.Exit(1)
+	}
+
+	logger.Info("shutdown complete")
 }
